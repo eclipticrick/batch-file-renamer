c8d96e0e245d91c48aa74d674c3dd2dc18f16b14
eclipticrick <eclipticrick@gmail.com>
2018-11-29 19:42:28 +0100

    - optimised is.js testing file

--- utils/is.spec.js
+++ utils/is.spec.js
@@ -1,6 +1,16 @@
 const is = require('./is');
 const { errorMessage } = is;
 
+const expectedTypeErrors = (fn, values, typeOfError) => {
+    values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
+    values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage[typeOfError]()));
+    values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage[typeOfError]()));
+    values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage[typeOfError]()));
+    values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage[typeOfError]()));
+    values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage[typeOfError]('path')));
+    values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage[typeOfError]('1')));
+};
+
 describe('is.js', () => {
     describe('is.required(value: any, path?: string): void', () => {
         const fn = is.required;
@@ -18,14 +28,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a string', () => {
             const values = [null, 0, 1234, true, {}, [], { id: 0 }, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.string()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.string('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.string()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.string()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.string('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.string('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.string()));
+            expectedTypeErrors(fn, values, 'string');
         });
     });
     describe('is.number(value: any, path?: string): void', () => {
@@ -36,14 +39,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a number', () => {
             const values = ['0', '1234', true, {}, [], { id: 0 }, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.number()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.number('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.number()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.number()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.number('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.number('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.number()));
+            expectedTypeErrors(fn, values, 'number');
         });
     });
     describe('is.boolean(value: any, path?: string): void', () => {
@@ -54,14 +50,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a number', () => {
             const values = ['0', '1234', -1, 0, 1, 0.3, {}, [], { id: 0 }, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.boolean()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.boolean('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.boolean()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.boolean()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.boolean('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.boolean('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.boolean()));
+            expectedTypeErrors(fn, values, 'boolean');
         });
     });
     describe('is.array(value: any, path?: string): void', () => {
@@ -72,14 +61,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a array', () => {
             const values = ['0', '1234', true, {}, 12345, null, { id: 0 }];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.array()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.array('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.array()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.array()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.array('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.array('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.array()));
+            expectedTypeErrors(fn, values, 'array');
         });
     });
     describe('is.object(value: any, path?: string): void', () => {
@@ -90,14 +72,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a object', () => {
             const values = ['0', '1234', true, [], -1, 0, 1, 0.3, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.object()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.object('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.object()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.object()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.object('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.object('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.object()));
+            expectedTypeErrors(fn, values, 'object');
         });
     });
     describe('is.stringOrNull(value: any, path?: string): void', () => {
@@ -108,14 +83,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a string or null', () => {
             const values = [0, 1234, true, {}, [], { id: 0 }, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.stringOrNull()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.stringOrNull('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.stringOrNull()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.stringOrNull()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.stringOrNull('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.stringOrNull('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.stringOrNull()));
+            expectedTypeErrors(fn, values, 'stringOrNull');
         });
     });
     describe('is.booleanOrNull(value: any, path?: string): void', () => {
@@ -126,14 +94,7 @@ describe('is.js', () => {
         });
         it('should throw a TypeError with the correct message if the given value was not a boolean or null', () => {
             const values = [0, 1234, '', 'abc', {}, [], { id: 0 }, [1, 2]];
-            values.forEach(value => expect(() => fn(value)).toThrow(TypeError));
-            values.forEach(value => expect(() => fn(value)).toThrowError(errorMessage.booleanOrNull()));
-            values.forEach(value => expect(() => fn(value, 'path')).toThrowError(errorMessage.booleanOrNull('path')));
-            values.forEach(value => expect(() => fn(value, '')).toThrowError(errorMessage.booleanOrNull()));
-            values.forEach(value => expect(() => fn(value, 0)).toThrowError(errorMessage.booleanOrNull()));
-            values.forEach(value => expect(() => fn(value, 1)).toThrowError(errorMessage.booleanOrNull('1')));
-            values.forEach(value => expect(() => fn(value, -1)).toThrowError(errorMessage.booleanOrNull('-1')));
-            values.forEach(value => expect(() => fn(value, null)).toThrowError(errorMessage.booleanOrNull()));
+            expectedTypeErrors(fn, values, 'booleanOrNull');
         });
     });
 
