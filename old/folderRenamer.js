085a81427175d7199798586be90662d333500eef
eclipticrick <eclipticrick@gmail.com>
2018-11-30 20:18:43 +0100

    - removed the old (unused) code

--- old/folderRenamer.js
+++
@@ -1,8 +0,0 @@
-const fn = require('./functions');
-
-const parentFolder = 'E:\\Some\\Folder';
-const newName = (oldName) => oldName.replace('.en.srt', '.srt');
-
-fn.readFolders(parentFolder, newName).then(() => fn.askToRenameFolders(parentFolder, newName));
-
-
